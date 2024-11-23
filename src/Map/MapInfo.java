df579f23a7f2973ba8e20d6e76cd2c4acccd2775
wodus4145@naver.com <wodus4145@naver.com>
2024-11-23 13:30:32 +0900

    refactor: MapInfo 클래스의 변수들과 getter, setter 메서드 추가

--- src/Map/MapInfo.java
+++ src/Map/MapInfo.java
@@ -2,11 +2,32 @@ package Map;
 
 public class MapInfo {
 
+    private int[][] mapData;
+    private int width;
+    private int height;
+    private int holeCount;  // 구멍 갯수
+    private int ballCount;  // 공 갯수
+    private int playerRow;  // 플레이어 행 위치
+    private int playerCol;  // 플레이어 열 위치
 
-//    String empty;
-//    String Hole;
-//    String Ball;
-//    String Player;
-//    String Wall;
+    public int[][] getMapData() { return mapData; }
+    public void setMapData(int[][] mapData) { this.mapData = mapData; }
 
+    public int getWidth() { return width; }
+    public void setWidth(int width) { this.width = width; }
+
+    public int getHeight() { return  height; }
+    public void setHeight(int height) { this.height = height; }
+
+    public int getHoleCount() { return holeCount; }
+    public void setHoleCount(int holeCount) { this.holeCount = holeCount; }
+
+    public int getBallCount() { return ballCount; }
+    public void setBallCount(int ballCount) { this.ballCount = ballCount; }
+
+    public int getPlayerRow() { return playerRow; }
+    public void setPlayerRow(int playerRow) { this.playerRow = playerRow; }
+
+    public int getPlayerCol() { return playerCol; }
+    public void setPlayerCol(int playerCol) { this.playerCol = playerCol; }
 }
