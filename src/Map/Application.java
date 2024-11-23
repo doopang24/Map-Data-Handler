d470df6d6f7de689c0167c6bf7c779c29b76092c
wodus4145@naver.com <wodus4145@naver.com>
2024-11-23 14:17:01 +0900

    feat: stage 정보를 문자열로 입력하는 메서드 작성

--- src/Map/Application.java
+++ src/Map/Application.java
@@ -5,9 +5,35 @@ import java.util.*;
 public class Application {
 
     MapInfo mapInfo = new MapInfo();
+    Converter converter = new Converter();
 
     public static void main(String[] args) {
         Application application = new Application();
+        String[] stages = application.receiveStages();
+        for (String singleStage : stages) {
+            application.parseStage(singleStage);
+        }
+    }
+
+    public String[] receiveStages() {  // 통채로 들어오는 String
+        String input = """
+                Stage 1
+                #####
+                #OoP#
+                #####
+                =====
+                Stage 2
+                  #######  
+                ###  O  ###
+                #    o    #
+                # Oo P oO #
+                ###  o  ###
+                  #  O  #  
+                  #######  
+                """;
+        String[] stages = input.split("=====");     // "="구분자로 나눠서 배열로 둠
+        return stages;
+    }
 
         application.receiveMapInfo();
 
